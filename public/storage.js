28dde6444a9fd1dd2720a2f15e1722bc758c5b14
ConstantlyCodingLLC <67442896+ConstantlyCodingLLC@users.noreply.github.com>
2025-10-16 23:44:25 -0500

    Delete public/storage.js

--- public/storage.js
+++
@@ -1,4 +0,0 @@
-const LS_KEY='payments_v3';
-function savePayments(payments){ localStorage.setItem(LS_KEY,JSON.stringify(payments)); }
-function loadPayments(){ return JSON.parse(localStorage.getItem(LS_KEY)||'[]'); }
-function generateId(){ return 'p_'+Math.random().toString(36).substr(2,9); }
